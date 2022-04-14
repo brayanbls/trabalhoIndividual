e6de5c022b9f3d24f2ea17b9a3fbb58968476216
brayanbls <brayanlaporta14@gmail.com>
2022-04-14 15:18:56 -0300

    Adiciona Categoria

---
+++ Produto.py
@@ -0,0 +1,14 @@
+class Produto:
+
+    def __init__ (self, cod = None, nomeP = None, precoP = 0.00, qtd = 0):
+        self.codigo = cod
+        self.nome = nomeP
+        self.preco = precoP
+        self.qtd = qtd
+
+    def Cadastrar(self):
+        print("Código: "),self.codigo
+        print("Nome: ",self.nome)
+        print("Preco: "),self.preco
+        print("Quantidade: ",self.qtd)
+        print("Produto cadastrado com sucesso!")
